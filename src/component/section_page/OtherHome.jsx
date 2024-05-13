eb718511e355cabe876d9e2a31d1acb316e87c24
jsy11428 <jsy11428@naver.com>
2024-05-13 17:37:58 +0900

    17:37

--- src/component/section_page/OtherHome.jsx
+++ src/component/section_page/OtherHome.jsx
@@ -21,7 +21,7 @@ const OtherHome = () => {
         console.log("otherHome useEffect()", member);
         
         // axios_get_search_member(member.M_ID);
-        axios_get_other_profile(member.M_ID);
+        // axios_get_other_profile(member.M_ID);
             
     },[]);
 
@@ -75,115 +75,115 @@ const OtherHome = () => {
     //     });
     
     // }
-    console.log('member.m_id: ', member.M_ID);
+    // console.log('member.m_id: ', member.M_ID);
 
-    const axios_get_other_profile = () => {
-        console.log('axios_get_other_profile()');
-        axios({
-            url: `${process.env.REACT_APP_HOST}/story/story/get_my_storys`,
-            method: 'get',
-            params: {
-                'm_id': member.M_ID,
-            },
-            headers: {
-                'authorization': sessionStorage.getItem('sessionID'),      
-            },
-        })
-        .then(response => {
-            console.log('AXIOS GET MY STORY COMMUNICATION SUCCESS');
-            console.log(response.data);
-            if (response.data === -1) {
-                console.log("Home session out!!");
-                sessionStorage.removeItem('sessionID');
-                dispatch({
-                    type: 'session_out',
-                });
-                navigate('/');
-            } else {
-                if (response.data === null) {
-                    console.log("undefined member");
-                    sessionStorage.removeItem('sessionID');
-                    dispatch({
-                        type: 'session_out',
-                    });
-                    navigate('/');
-                } else {
-                    dispatch({
-                        type: 'set_my_stories',
-                        story: response.data,
-                    });
+    // const axios_get_other_profile = () => {
+    //     console.log('axios_get_other_profile()');
+    //     axios({
+    //         url: `${process.env.REACT_APP_HOST}/story/story/get_my_storys`,
+    //         method: 'get',
+    //         params: {
+    //             'm_id': member.M_ID,
+    //         },
+    //         headers: {
+    //             'authorization': sessionStorage.getItem('sessionID'),      
+    //         },
+    //     })
+    //     .then(response => {
+    //         console.log('AXIOS GET MY STORY COMMUNICATION SUCCESS');
+    //         console.log(response.data);
+    //         if (response.data === -1) {
+    //             console.log("Home session out!!");
+    //             sessionStorage.removeItem('sessionID');
+    //             dispatch({
+    //                 type: 'session_out',
+    //             });
+    //             navigate('/');
+    //         } else {
+    //             if (response.data === null) {
+    //                 console.log("undefined member");
+    //                 sessionStorage.removeItem('sessionID');
+    //                 dispatch({
+    //                     type: 'session_out',
+    //                 });
+    //                 navigate('/');
+    //             } else {
+    //                 dispatch({
+    //                     type: 'set_my_stories',
+    //                     story: response.data,
+    //                 });
                     
-                }
-            }
+    //             }
+    //         }
             
-        })
-        .catch(error => {
-            console.log('AXIOS GET MY STORY COMMUNICATION ERROR', error);
-        })
-        .finally(() => {
-            console.log('AXIOS GET MY STORY COMMUNICATION COMPLETE');
-            sessionStorage.removeItem('sessionID');//
-            sessionStorage.setItem('sessionID',getCookie('accessToken'));//
-        });
-    }
+    //     })
+    //     .catch(error => {
+    //         console.log('AXIOS GET MY STORY COMMUNICATION ERROR', error);
+    //     })
+    //     .finally(() => {
+    //         console.log('AXIOS GET MY STORY COMMUNICATION COMPLETE');
+    //         sessionStorage.removeItem('sessionID');//
+    //         sessionStorage.setItem('sessionID',getCookie('accessToken'));//
+    //     });
+    // }
 
-    const axios_get_friend = (m_id) => {
-        console.log('axios_get_friend()');
-        axios({
-            url: `${process.env.REACT_APP_HOST}/member/get_friend_list`,
-            method: 'get',
-            params: {
-                'm_id': m_id
-            },
-            headers: {
-                'authorization': sessionStorage.getItem('sessionID'),      
-            }, 
-        })
-        .then(response => {
-                console.log('AXIOS GET MY STORY COMMUNICATION SUCCESS');
-                console.log(response.data);
-                if (response.data === -1) {
-                    console.log("Home session out!!");
-                    sessionStorage.removeItem('sessionID');
-                    dispatch({
-                        type: 'session_out',
-                    });
-                    navigate('/');
-                } else {
-                    if (response.data === null) {
-                        console.log("undefined member");
-                        sessionStorage.removeItem('sessionID');
-                        dispatch({
-                            type: 'session_out',
-                        });
-                        navigate('/');
-                    } else {
+    // const axios_get_friend = (m_id) => {
+    //     console.log('axios_get_friend()');
+    //     axios({
+    //         url: `${process.env.REACT_APP_HOST}/member/get_friend_list`,
+    //         method: 'get',
+    //         params: {
+    //             'm_id': m_id
+    //         },
+    //         headers: {
+    //             'authorization': sessionStorage.getItem('sessionID'),      
+    //         }, 
+    //     })
+    //     .then(response => {
+    //             console.log('AXIOS GET MY STORY COMMUNICATION SUCCESS');
+    //             console.log(response.data);
+    //             if (response.data === -1) {
+    //                 console.log("Home session out!!");
+    //                 sessionStorage.removeItem('sessionID');
+    //                 dispatch({
+    //                     type: 'session_out',
+    //                 });
+    //                 navigate('/');
+    //             } else {
+    //                 if (response.data === null) {
+    //                     console.log("undefined member");
+    //                     sessionStorage.removeItem('sessionID');
+    //                     dispatch({
+    //                         type: 'session_out',
+    //                     });
+    //                     navigate('/');
+    //                 } else {
 
-                        if (response.data === loginedMember) {
-                            dispatch({
-                                type:'set_my_friend',
-                                button: -2,
-                            }) 
-                        } else {
-                            dispatch({
-                                type:'set_my_friend',
-                                button: -1,
-                            })
-                        }
+    //                     if (response.data === loginedMember) {
+    //                         dispatch({
+    //                             type:'set_my_friend',
+    //                             button: -2,
+    //                         }) 
+    //                     } else {
+    //                         dispatch({
+    //                             type:'set_my_friend',
+    //                             button: -1,
+    //                         })
+    //                     }
                         
-                    }
-                }
+    //                 }
+    //             }
             
-            })
-            .catch(error => {
-                console.log('AXIOS GET MY STORY COMMUNICATION ERROR', error);
-            })
-            .finally(() => {
-                console.log('AXIOS GET MY STORY COMMUNICATION COMPLETE');
-                sessionStorage.removeItem('sessionID');//
-                sessionStorage.setItem('sessionID',getCookie('accessToken'));//
-            });
-        }
+    //         })
+    //         .catch(error => {
+    //             console.log('AXIOS GET MY STORY COMMUNICATION ERROR', error);
+    //         })
+    //         .finally(() => {
+    //             console.log('AXIOS GET MY STORY COMMUNICATION COMPLETE');
+    //             sessionStorage.removeItem('sessionID');//
+    //             sessionStorage.setItem('sessionID',getCookie('accessToken'));//
+    //         });
+    //     }
     
 
     return (
